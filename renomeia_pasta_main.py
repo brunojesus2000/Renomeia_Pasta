ebab8789b72f64d1c51d16134a01d18e3e2fdd4d
Bruno JP <brunojpsk8@gmail.com>
2016-01-31 22:21:51 -0200

    Criação do arquivo main de renomeia_pasta
    Neste passo eu criei um arquivo e fiz uma operação de teste.

---
+++ renomeia_pasta_main.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+
+#Este é um teste de um programa que renomea pastas e arquivos retirando caracteres especiais
+
+import os, sys, shutil
+
+caminho_atual = raw_input("Insira o caminho do diretorio: ")
+
+print caminho_atual
