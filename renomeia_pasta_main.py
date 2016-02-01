e780aab90f93b14c7ebe2e3e3335f55578d2a5f5
Bruno JP <brunojpsk8@gmail.com>
2016-01-31 22:50:17 -0200

    Primeiro trabalho com o codigo

--- renomeia_pasta_main.py
+++ renomeia_pasta_main.py
@@ -1,9 +1,19 @@
 # -*- coding: UTF-8 -*-
 
 #Este é um teste de um programa que renomea pastas e arquivos retirando caracteres especiais
+# Diretorio teste: /Users/bruno/Desktop/DirTeste
 
 import os, sys, shutil
 
 caminho_atual = raw_input("Insira o caminho do diretorio: ")
 
-print caminho_atual
+print "Varrendo Caminho: " + caminho_atual
+
+diretorio = os.walk(caminho_atual)
+diretorio_espelho = []
+#Adiciona os caminhos em um espelho para renomear depois
+for subdir in diretorio:
+    diretorio_espelho.append(str(subdir[0]))
+#Teste para printar diretorios
+for pasta in diretorio_espelho:
+    print pasta
