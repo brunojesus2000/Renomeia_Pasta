cfe1e44c9b3e4aa478b5569612c33d1aa385b5de
Bruno JP <brunojesus2000@github.com>
2016-02-05 23:14:18 -0200

    Adicionado script que renomeie os arquivos de pastas e subpastas
    Faltam apenas os retoques finais

--- renomeia_pasta_main.py
+++ renomeia_pasta_main.py
@@ -21,19 +21,37 @@ def varre_diretorio(dir_atual):
 
 diretorio_espelho = varre_diretorio(caminho_atual)
 
-cont_dir = len(diretorio_espelho)
-print cont_dir
 
+cont_list = len(diretorio_espelho)
+cont_dir = 0
+cont_items = 0
 
 #Teste para printar diretorios
-for pasta in range(cont_dir):
+for pasta in range(cont_list):
 	if (pasta == 0) or (pasta == cont_dir):
 
 		print True
 	else:
+		cont_dir += 1
 		pasta_old = diretorio_espelho[pasta]
-		pasta_new = pasta_old.translate(None, 'ã\'$*çé́?̃\%_-^̂' )
+		pasta_new = pasta_old.translate(None, 'ã\'$\"*çé́?̃\%_-^̂' )
 		print pasta_old + " indo para " + pasta_new
 		os.rename(pasta_old, pasta_new)
 		diretorio_espelho = varre_diretorio(caminho_atual)
 		print pasta_old + " == " + pasta_new
+
+#Teste para renomear arquivos
+for pasta in os.walk(caminho_atual):
+	for item in pasta[2]:
+		cont_items += 1
+		item_old = pasta[0] + "/" + str(item)
+		item_new = item_old.translate(None, 'ã\'$\"*çé́?̃\%_-^̂' )
+		print item_old + " indo para " + item_new
+		os.rename(item_old, item_new)
+		print item_old + " == " + item_new
+
+print "____________________________________________________________________________________"
+print "Finalizado com sucesso!"
+print "Foram varridos:"
+print "%d diretorios e %d arquivos" % (cont_dir,cont_items)
+print "____________________________________________________________________________________"
